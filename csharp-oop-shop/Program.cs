6ab9b672a9bf748b1386225c0ebeba44e9a44c43
R-G2110 <argeldelacruz86@gmail.com>
2024-04-19 14:32:46 +0200

    [Add]: class Product with attributes

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,5 +1,13 @@
 ﻿namespace csharp_oop_shop
 {
+    class Prodotto
+    {
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private decimal prezzo;
+        private decimal iva;
+    }
     internal class Program
     {
         static void Main(string[] args)
